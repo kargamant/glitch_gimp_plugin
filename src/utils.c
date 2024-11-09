8c6d9324730d2077242d3f93a01277488f30b966
kargamant <firexholms@gmail.com>
2024-11-09 17:54:25 +0300

    Made random rgn func

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,25 @@
 #include "utils.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void get_random_rgn(GimpDrawable* dr, GimpPixelRgn* rgin, GimpPixelRgn* rgout)
+{
+	gint x1, y1, x2, y2;
+	gimp_drawable_mask_bounds(dr->drawable_id, &x1, &y1, &x2, &y2);
+	
+	srand(time(NULL));
+	int lx=x1+rand()%x2;
+	int ly=y1+rand()%y2;
+	int rx=lx+rand()%(x2-lx);
+	int ry=ly+rand()%(y2-ly);
+
+	printf("image bounds: %d, %d, %d, %d\n", x1, y1, x2, y2);
+	printf("coordinates: %d, %d, %d, %d\n", lx, ly, rx, ry);
+	
+	gimp_pixel_rgn_init(rgin, dr, lx, ly, rx-lx, ry-ly, FALSE, FALSE);
+	gimp_pixel_rgn_init(rgout, dr, lx, ly, rx-lx, ry-ly, TRUE, FALSE);
+}
 
 void inverse_pixels(GimpDrawable* dr)
 {
@@ -14,16 +34,17 @@ void inverse_pixels(GimpDrawable* dr)
 	
 	//initializing regions
 	printf("initializing regions\n");
-	gimp_pixel_rgn_init(&rgin, dr, x1, y1, x2-x1, y2-y1, FALSE, FALSE);
-	gimp_pixel_rgn_init(&rgout, dr, x1, y1, x2-x1, y2-y1, TRUE, TRUE);
+	get_random_rgn(dr, &rgin, &rgout);
+	
+	int width=rgin.w;
+	int height=rgin.h;
+	guchar* buff=g_new(guchar, width*channels);
+	printf("Prepared buffer\n");
 	
 	// test modification
-	int width=1000;
-	guchar* buff=g_new(guchar, (x2-x1)*channels);
-	printf("Prepared buffer\n");
-	for(int i=0; i<width; i++)
+	for(int i=0; i<height; i++)
 	{
-		gimp_pixel_rgn_get_row(&rgin, buff, 0, i, x2-x1);
+		gimp_pixel_rgn_get_row(&rgin, buff, rgin.x, rgin.y+i, width);
 		for(int k=0; k<width; k++)
 		{
 			for(int j=0; j<channels; j++)
@@ -31,23 +52,14 @@ void inverse_pixels(GimpDrawable* dr)
 				buff[k*channels+j]=150;
 			}
 		}
-		gimp_pixel_rgn_set_row(&rgout, buff, 0, i, x2-x1);
-	}
-	guchar* addition = g_new(guchar, (x2-x1)*channels);
-	for(int h=width; h<y2-y1; h++)
-	{
-		gimp_pixel_rgn_get_row(&rgin, addition, 0, h, x2-x1);
-		gimp_pixel_rgn_set_row(&rgout, addition, 0, h, x2-x1);
+		gimp_pixel_rgn_set_row(&rgout, buff, rgin.x, rgin.y+i, width);
 	}
 	printf("Processing finished\n");
 	
-	g_free(addition);
 	g_free(buff);
 
 	gimp_drawable_flush(dr);
 	printf("data flushed\n");
-
-	gimp_drawable_merge_shadow(dr->drawable_id, TRUE);
 
 	gimp_drawable_update(dr->drawable_id, x1, y1, x2-x1, y2-y1);
 	printf("drawable updated\n");
--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,6 @@
 #include <libgimp/gimp.h>
 
 void inverse_pixels(GimpDrawable* dr);
+void shift_pixels(GimpDrawable* dr);
+void color_corrupt(GimpDrawable* dr);
+void get_random_rgn(GimpDrawable* dr, GimpPixelRgn* rgin, GimpPixelRgn* rgout);
